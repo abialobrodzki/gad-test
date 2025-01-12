74264346030ba6ccbd934701b2ae00ebdfab5936
adrianbialobrodzki <adrian.bialobrodzki@gmail.com>
2025-01-12 17:51:01 +0100

    chore: separate describe for title length

--- tests/article.spec.ts
+++ tests/article.spec.ts
@@ -63,27 +63,29 @@ test.describe('Verify article', () => {
     await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage)
   })
 
-  test('reject creating article with title exceeding 128 signs @GAD-R04-02', async () => {
-    // Arrange
-    const expectedErrorMessage = 'Article was not created'
-    const articleData = randomNewArticle(129)
+  test.describe('title length', () => {
+    test('reject creating article with title exceeding 128 signs @GAD-R04-02', async () => {
+      // Arrange
+      const expectedErrorMessage = 'Article was not created'
+      const articleData = randomNewArticle(129)
 
-    // Act
-    await addArticleView.createArticle(articleData)
+      // Act
+      await addArticleView.createArticle(articleData)
 
-    // Assert
-    await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage)
-  })
+      // Assert
+      await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage)
+    })
 
-  test('create article with title with 128 signs @GAD-R04-02', async ({ page }) => {
-    // Arrange
-    const articlePage = new ArticlePage(page)
-    const articleData = randomNewArticle(128)
+    test('create article with title with 128 signs @GAD-R04-02', async ({ page }) => {
+      // Arrange
+      const articlePage = new ArticlePage(page)
+      const articleData = randomNewArticle(128)
 
-    // Act
-    await addArticleView.createArticle(articleData)
+      // Act
+      await addArticleView.createArticle(articleData)
 
-    // Assert
-    await expect(articlePage.articleTitle).toHaveText(articleData.title)
+      // Assert
+      await expect(articlePage.articleTitle).toHaveText(articleData.title)
+    })
   })
 })
