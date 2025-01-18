9a5d1288daedb72848000ef5c930d371ecf992ac
adrianbialobrodzki <adrian.bialobrodzki@gmail.com>
2025-01-18 02:38:09 +0100

    chore: refactor to aaa formatting

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,11 +9,12 @@ test.describe('Verify login', () => {
     // Arrange
     const expectedWelcomeTitle = 'Welcome'
     const loginPage = new LoginPage(page)
+    const welcomePage = new WelcomePage(page)
 
     // Act
     await loginPage.goto()
     await loginPage.login(testUser1)
-    const welcomePage = new WelcomePage(page)
+
     const title = await welcomePage.getTitle()
 
     // Assert
@@ -25,11 +26,11 @@ test.describe('Verify login', () => {
     const expectedLoginTitle = 'Login'
     const expectedErrorMessage = 'Invalid username or password'
 
+    const loginPage = new LoginPage(page)
     const loginUserData: LoginUserModel = {
       userEmail: testUser1.userEmail,
       userPassword: 'incorrectPassword',
     }
-    const loginPage = new LoginPage(page)
 
     // Act
     await loginPage.goto()
