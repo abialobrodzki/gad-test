b4172cb681502024567877b5d51ab2fc6101f37d
adrianbialobrodzki <adrian.bialobrodzki@gmail.com>
2025-01-26 18:52:43 +0100

    chore: refactor register action to return PO

--- tests/integration/register.spec.ts
+++ tests/integration/register.spec.ts
@@ -1,6 +1,5 @@
 import { prepareRandomUser } from '@_src/factories/user.factory'
 import { RegisterUserModel } from '@_src/models/user.model'
-import { LoginPage } from '@_src/pages/login.page'
 import { RegisterPage } from '@_src/pages/register.page'
 import { expect, test } from '@playwright/test'
 
@@ -14,16 +13,14 @@ test.describe('Verify register', () => {
     await registerPage.goto()
   })
 
-  test('register with correct data and login @GAD-R03-01 @GAD-R03-02 @GAD-R03-03', async ({ page }) => {
+  test('register with correct data and login @GAD-R03-01 @GAD-R03-02 @GAD-R03-03', async ({}) => {
     // Arrange
     const expectedAlertPopupText = 'User created'
     const expectedLoginTitle = 'Login'
     const expectedWelcomeTitle = 'Welcome'
 
-    const loginPage = new LoginPage(page)
-
     // Act
-    await registerPage.register(registerUserData)
+    const loginPage = await registerPage.register(registerUserData)
 
     // Assert
     await expect(registerPage.alertPopup).toHaveText(expectedAlertPopupText)
