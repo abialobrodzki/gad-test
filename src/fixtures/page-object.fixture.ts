2c37b6b3b344ab2162d8b9170082d032a3d7a813
adrianbialobrodzki <adrian.bialobrodzki@gmail.com>
2025-01-29 22:19:47 +0100

    chore: own fixtures in articles tests

--- src/fixtures/page-object.fixture.ts
+++ src/fixtures/page-object.fixture.ts
@@ -1,12 +1,16 @@
+import { ArticlePage } from '@_src/pages/article.page'
 import { ArticlesPage } from '@_src/pages/articles.page'
 import { CommentsPage } from '@_src/pages/comments.page'
 import { HomePage } from '@_src/pages/home.page'
 import { LoginPage } from '@_src/pages/login.page'
 import { RegisterPage } from '@_src/pages/register.page'
+import { AddArticleView } from '@_src/views/add-article.view'
 import { test as baseTest } from '@playwright/test'
 
 //dodatkowy interfejs 'Pages'
 interface Pages {
+  addArticleView: AddArticleView
+  articlePage: ArticlePage
   articlesPage: ArticlesPage
   commentsPage: CommentsPage
   homePage: HomePage
@@ -16,9 +20,17 @@ interface Pages {
 
 //obiekt testowy zawierający fixtures
 export const pageObjectTest = baseTest.extend<Pages>({
+  addArticleView: async ({ articlesPage }, use) => {
+    const addArticleView = await articlesPage.clickAddArticleButtonLogged()
+    await use(addArticleView)
+  },
+  articlePage: async ({ page }, use) => {
+    const articlePage = new ArticlePage(page)
+    await articlePage.goto()
+    await use(articlePage)
+  },
   articlesPage: async ({ page }, use) => {
     const articlesPage = new ArticlesPage(page)
-    await articlesPage.goto()
     await use(articlesPage)
   },
   commentsPage: async ({ page }, use) => {
