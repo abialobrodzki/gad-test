ad0bdb868082ff66b36b2a9e3714db99a7d27571
adrianbialobrodzki <adrian.bialobrodzki@gmail.com>
2025-02-09 23:08:27 +0100

    test: comment deletion

--- tests/api/comments.intergration.spec.ts
+++ tests/api/comments.intergration.spec.ts
@@ -1,13 +1,20 @@
 import { expect, test } from '@_src/fixtures/merge.fixture'
-import { apiLinks, getAuthorizationHeader, prepareArticlePayload, prepareCommentPayload } from '@_src/utils/api.util'
+import {
+  CommentPayload,
+  Headers,
+  apiLinks,
+  getAuthorizationHeader,
+  prepareArticlePayload,
+  prepareCommentPayload,
+} from '@_src/utils/api.util'
+import { APIResponse } from '@playwright/test'
 
 test.describe('Verify comments CRUD operations @crud', () => {
   let articleId: number
-  let headers: { [key: string]: string }
+  let headers: Headers
 
   test.beforeAll('create an article', async ({ request }) => {
     headers = await getAuthorizationHeader(request)
-
     //create article
     const articleData = prepareArticlePayload()
 
@@ -34,27 +41,200 @@ test.describe('Verify comments CRUD operations @crud', () => {
     //Assert
     expect(response.status(), `Expected status code: "${expectedStatusCode}"`).toBe(expectedStatusCode)
   })
-  test('should create a comment with logged-in user @GAD-R08-04', async ({ request }) => {
-    // Arrange
-    const expectedStatusCode = 201
 
-    // Act
-    const commentData = prepareCommentPayload(articleId)
+  //testy zależne z usuwaniem komentarza
+  test.describe.configure({ mode: 'serial' })
+  test.describe('CRUD operations - serial', () => {
+    let commentId: number
+    let responseComment: APIResponse
+    let commentData: CommentPayload
 
-    // Arrange
-    const response = await request.post(apiLinks.commentsUrl, {
-      headers,
-      data: commentData,
+    test.beforeAll('should login', async ({ request }) => {
+      headers = await getAuthorizationHeader(request)
+    })
+    test('should create an article with logged-in user @GAD-R08-04', async ({ request }) => {
+      // Arrange
+      commentData = prepareCommentPayload(articleId)
+      responseComment = await request.post(apiLinks.commentsUrl, {
+        headers,
+        data: commentData,
+      })
+      //dodanie oczekiwania na 5 sek - 'ulep' xD
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+      const expectedStatusCode = 201
+
+      // Assert
+      const actualResponseStatus = responseComment.status()
+      expect(
+        actualResponseStatus,
+        `expected status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode)
+      //sprawdzenie utworzonego komentarza
+      const commentJson = await responseComment.json()
+      expect.soft(commentJson.body).toEqual(commentData.body)
+
+      commentId = commentJson.id
     })
 
-    //Assert
-    const actualResponseStatus = response.status()
-    expect(
-      actualResponseStatus,
-      `expected status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
-    ).toBe(expectedStatusCode)
+    test('should not delete an article with non logged-in user @GAD-R08-06', async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 401
 
-    const comment = await response.json()
-    expect.soft(comment.body).toEqual(commentData.body)
+      // Act
+      const responseCommentNotDeleted = await request.delete(`${apiLinks.commentsUrl}/${commentId}`, {})
+
+      // Assert
+      const actualResponseStatus = responseCommentNotDeleted.status()
+      expect(
+        actualResponseStatus,
+        `expected status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode)
+
+      // Assert check not deleted comment response
+      const expectedNotDeletedCommentStatusCode = 200
+      const responseCommentNotDeletedGet = await request.get(`${apiLinks.commentsUrl}/${commentId}`)
+
+      expect(
+        responseCommentNotDeletedGet.status(),
+        `expected status code ${expectedNotDeletedCommentStatusCode}, and received ${responseCommentNotDeletedGet.status()}`,
+      ).toBe(expectedNotDeletedCommentStatusCode)
+    })
+
+    test('should delete an article with logged-in user @GAD-R08-06', async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 200
+
+      // Act
+      const responseCommentDeleted = await request.delete(`${apiLinks.commentsUrl}/${commentId}`, {
+        headers,
+      })
+
+      // Assert
+      const actualResponseStatus = responseCommentDeleted.status()
+      expect(
+        actualResponseStatus,
+        `expected status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode)
+
+      // Assert check deleted comment response
+      const expectedDeletedCommentStatusCode = 404
+      const responseCommentDeletedGet = await request.get(`${apiLinks.commentsUrl}/${commentId}`, { headers })
+
+      expect(
+        responseCommentDeletedGet.status(),
+        `expected status code ${expectedDeletedCommentStatusCode}, and received ${responseCommentDeletedGet.status()}`,
+      ).toBe(expectedDeletedCommentStatusCode)
+
+      // Assert check comment deleted
+      const expectedReturnObject = {}
+      const retrieveResponse = await request.get(`${apiLinks.commentsUrl}/${commentId}`, {
+        headers,
+      })
+      const actualStatusCode = retrieveResponse.status()
+      const retrieveResponseJSON = await retrieveResponse.json()
+
+      expect(
+        actualStatusCode,
+        `Expected status code ${expectedDeletedCommentStatusCode}, and received ${actualStatusCode} !`,
+      ).toBe(expectedDeletedCommentStatusCode)
+      expect(retrieveResponseJSON).toEqual(expectedReturnObject)
+    })
+  })
+
+  //testy niezależne z usuwaniem komentarza
+  test.describe('CRUD operations', () => {
+    let responseComment: APIResponse
+    let commentData: CommentPayload
+
+    test.beforeEach('create comment', async ({ request }) => {
+      commentData = prepareCommentPayload(articleId)
+      responseComment = await request.post(apiLinks.commentsUrl, {
+        headers,
+        data: commentData,
+      })
+      //dodanie oczekiwania na 5 sek - 'ulep' xD
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+    })
+
+    test('should create a comment with logged-in user @GAD-R08-04', async () => {
+      // Arrange
+      const expectedStatusCode = 201
+
+      // Assert
+      const actualResponseStatus = responseComment.status()
+      expect(
+        actualResponseStatus,
+        `expected status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode)
+      //sprawdzenie utworzonego komentarza
+      const commentJson = await responseComment.json()
+      expect.soft(commentJson.body).toEqual(commentData.body)
+    })
+
+    test('should delete a comment with logged-in user @GAD-R08-06', async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 200
+      const comment = await responseComment.json()
+
+      // Act
+      const responseCommentDeleted = await request.delete(`${apiLinks.commentsUrl}/${comment.id}`, {
+        headers,
+      })
+
+      // Assert
+      const actualResponseStatus = responseCommentDeleted.status()
+      expect(
+        actualResponseStatus,
+        `expected status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode)
+
+      // Assert check deleted comment response
+      const expectedDeletedCommentStatusCode = 404
+      const responseCommentDeletedGet = await request.get(`${apiLinks.commentsUrl}/${comment.id}`, { headers })
+
+      expect(
+        responseCommentDeletedGet.status(),
+        `expected status code ${expectedDeletedCommentStatusCode}, and received ${responseCommentDeletedGet.status()}`,
+      ).toBe(expectedDeletedCommentStatusCode)
+
+      // Assert check comment deleted
+      const expectedReturnObject = {}
+      const retrieveResponse = await request.get(`${apiLinks.commentsUrl}/${comment.id}`, {
+        headers,
+      })
+      const actualStatusCode = retrieveResponse.status()
+      const retrieveResponseJSON = await retrieveResponse.json()
+
+      expect(
+        actualStatusCode,
+        `Expected status code ${expectedDeletedCommentStatusCode}, and received ${actualStatusCode} !`,
+      ).toBe(expectedDeletedCommentStatusCode)
+      expect(retrieveResponseJSON).toEqual(expectedReturnObject)
+    })
+
+    test('should not delete a comment with non logged-in user @GAD-R08-06', async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 401
+      const comment = await responseComment.json()
+
+      // Act
+      const responseCommentNotDeleted = await request.delete(`${apiLinks.commentsUrl}/${comment.id}`, {})
+
+      // Assert
+      const actualResponseStatus = responseCommentNotDeleted.status()
+      expect(
+        actualResponseStatus,
+        `expected status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode)
+
+      // Assert check not deleted comment response
+      const expectedNotDeletedCommentStatusCode = 200
+      const responseCommentNotDeletedGet = await request.get(`${apiLinks.commentsUrl}/${comment.id}`)
+
+      expect(
+        responseCommentNotDeletedGet.status(),
+        `expected status code ${expectedNotDeletedCommentStatusCode}, and received ${responseCommentNotDeletedGet.status()}`,
+      ).toBe(expectedNotDeletedCommentStatusCode)
+    })
   })
 })
